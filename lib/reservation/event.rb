d1997c9ec037553ff7d27086a6bf133539d7faeb
conanite <conan@conandalton.net>
2013-07-11 19:38:57 +0200

    events: #since and #upto scopes

--- lib/reservation/event.rb
+++ lib/reservation/event.rb
@@ -2,6 +2,9 @@ class Reservation::Event < ActiveRecord::Base
   has_many :reservations, :class_name => "Reservation::Reservation"
   attr_accessible :finish, :start, :title
 
+  scope :since, lambda { |time| where("reservation_events.finish > ?", time) }
+  scope :upto,  lambda { |time| where("reservation_events.start  < ?", time) }
+
   def overlap? range_start, range_end
     (range_end > start) && (range_start < finish)
   end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -29,4 +29,94 @@ describe Reservation::Event do
       event.overlap?(Time.now - 18.hours, Time.now - 9.hours).should == true
     end
   end
+
+  describe "#since and #upto scopes" do
+    before {
+      @r1 = Reservation::Event.create! :start => time("1999-12-15T16:00:00"), :finish => time("1999-12-15T22:00:00"), :title => "r1"
+      @r2 = Reservation::Event.create! :start => time("1999-12-15T08:00:00"), :finish => time("1999-12-15T22:00:00"), :title => "r2"
+      @r3 = Reservation::Event.create! :start => time("1999-12-14T00:00:00"), :finish => time("1999-12-25T00:00:00"), :title => "r3"
+      @r4 = Reservation::Event.create! :start => time("1999-12-01T18:00:00"), :finish => time("1999-12-01T22:00:00"), :title => "r4"
+      @r5 = Reservation::Event.create! :start => time("1999-12-01T00:00:00"), :finish => time("1999-12-15T19:00:00"), :title => "r5"
+      @r6 = Reservation::Event.create! :start => time("1999-12-15T17:00:00"), :finish => time("1999-12-16T07:00:00"), :title => "r6"
+
+      #r1                                     ======
+      #r2                             ==============
+      #r3                 ==========================================
+      #r4    =====
+      #r5 =======================================
+      #r6                                      ============
+    }
+
+    def since t
+      Reservation::Event.since(time(t)).order(:id).map(&:title)
+    end
+
+    def upto t
+      Reservation::Event.upto(time(t)).order(:id).map(&:title)
+    end
+
+    it "should find all events since 1999-01-01 00:00" do
+      since("1999-01-01T00:00:00").should == %w{ r1 r2 r3 r4 r5 r6 }
+    end
+
+    it "should find 6 events since 1999-12-01 16:00" do
+      since("1999-12-01T16:00:00").should == %w{ r1 r2 r3 r4 r5 r6 }
+    end
+
+    it "should find 5 events since 1999-12-15 16:00" do
+      since("1999-12-15T16:00:00").should == %w{ r1 r2 r3 r5 r6 }
+    end
+
+    it "should find 5 events since 1999-12-15 18:00" do
+      since("1999-12-15T18:00:00").should == %w{ r1 r2 r3 r5 r6 }
+    end
+
+    it "should find 4 events since 1999-12-15 20:00" do
+      since("1999-12-15T20:00:00").should == %w{ r1 r2 r3 r6 }
+    end
+
+    it "should find 2 events since 1999-12-15 23:00" do
+      since("1999-12-15T23:00:00").should == %w{ r3 r6 }
+    end
+
+    it "should find 1 event since 1999-12-16 08:00" do
+      since("1999-12-16T08:00:00").should == %w{ r3 }
+    end
+
+    it "should find no event since 1999-12-26 08:00" do
+      since("1999-12-26T08:00:00").should == []
+    end
+
+    it "should find all events upto 1999-12-26 00:00" do
+      upto("1999-12-26T00:00:00").should == %w{ r1 r2 r3 r4 r5 r6 }
+    end
+
+    it "should find 6 events upto 1999-12-24 12:00" do
+      upto("1999-12-24T12:00:00").should == %w{ r1 r2 r3 r4 r5 r6 }
+    end
+
+    it "should find 5 events upto 1999-12-15 17:00" do
+      upto("1999-12-15T17:00:00").should == %w{ r1 r2 r3 r4 r5 }
+    end
+
+    it "should find 4 events upto 1999-12-15 16:00" do
+      upto("1999-12-15T16:00:00").should == %w{ r2 r3 r4 r5 }
+    end
+
+    it "should find 3 events upto 1999-12-15 08:00" do
+      upto("1999-12-15T08:00:00").should == %w{ r3 r4 r5 }
+    end
+
+    it "should find 2 events upto 1999-12-07 12:00" do
+      upto("1999-12-07T12:00:00").should == %w{ r4 r5 }
+    end
+
+    it "should find 1 event upto 1999-12-01 18:00" do
+      upto("1999-12-01T18:00:00").should == %w{ r5 }
+    end
+
+    it "should find no event upto 1999-11-30 23:00" do
+      upto("1999-11-30T23:00:00").should == []
+    end
+  end
 end
