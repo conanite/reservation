b0c6b9b6caa3f3985442db19d4b5335f5b92ab91
conanite <conan@conandalton.net>
2013-07-11 19:37:46 +0200

    spec: clean db before each test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,34 @@ RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.order = 'random'
+
+  config.before(:each) {
+    models = [Thing, Place, Reservation::Event, Reservation::Reservation]
+    models.each do |model|
+      ActiveRecord::Base.connection.execute "DELETE FROM #{model.table_name}"
+    end
+  }
+end
+
+
+module Helper
+  def date d
+    Date.parse d
+  end
+
+  def time t
+    Time.parse t
+  end
+end
+
+RSpec::Core::ExampleGroup.send :include, Helper
+
+File.delete File.dirname(__FILE__) + '/dummy/db/test.sqlite3'
+ActiveRecord::Base.establish_connection
+ActiveRecord::Base.connection
+ActiveRecord::Base.silence do
+  ActiveRecord::Migration.verbose = false
+  load(File.dirname(__FILE__) + '/dummy/db/schema.rb')
 end
 
 
