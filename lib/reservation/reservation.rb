a26906f7a4efa759c96035be14ed821eebeda21e
conanite <conan@conandalton.net>
2013-07-12 15:33:14 +0200

    reservation: define module and class separately

--- lib/reservation/reservation.rb
+++ lib/reservation/reservation.rb
@@ -1,5 +1,7 @@
-class Reservation::Reservation < ActiveRecord::Base
-  belongs_to :event, :class_name => "Reservation::Event"
-  belongs_to :subject, :polymorphic => true
-  attr_accessible :reservation_status, :role, :subject_id, :subject_type, :event_id, :event, :subject
+module Reservation
+  class Reservation < ActiveRecord::Base
+    belongs_to :event, :class_name => "Reservation::Event"
+    belongs_to :subject, :polymorphic => true
+    attr_accessible :reservation_status, :role, :subject_id, :subject_type, :event_id, :event, :subject
+  end
 end
