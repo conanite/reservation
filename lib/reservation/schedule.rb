a3ca011515d5c93abfa56d0204c5805b32de1b50
conanite <conan@conandalton.net>
2013-07-12 11:34:49 +0200

    scheduling: add #parse to HourMinute class

--- lib/reservation/schedule.rb
+++ lib/reservation/schedule.rb
@@ -1,7 +1,10 @@
 module Reservation
-  DAY_MAP = %w{ sun mon tue wed thu fri sat }
+  DAY_MAP = { "sun" => 0, "mon" => 1, "tue" => 2, "wed" => 3, "thu" => 4, "fri" => 5, "sat" => 6}
 
   module Schedule
+    #
+    # a utility class to match the hour and minute elements of a Time instance, without considering any other values
+    #
     class HourMinute
       attr_accessor :hour, :minute
       def initialize hour, minute
@@ -11,8 +14,36 @@ module Reservation
       def matches_time? time
         time.hour == self.hour && time.min == self.minute
       end
+
+      #
+      # hhmm is a string containg an hour-and-minute value
+      #
+      # #parse will remove all nondigit characters, pad the result to 4 digits,
+      # and interpret the first two as an hour value, the last two as a minute value
+      #
+      # padding takes place as follows :
+      #  * one digit becomes 0d00 (assumes "7" means "0700")
+      #  * two digits become dd00 (assumes "11" means "1100")
+      #  * three digits become 0ddd (assumes "830" means "0830")
+      #
+      def self.parse hhmm
+        orig = hhmm
+        hhmm = hhmm.gsub /[^\d]/, ""
+        hhmm = "0#{hhmm}00" if hhmm.length == 1
+        hhmm = "#{hhmm}00" if hhmm.length == 2
+        hhmm = "0#{hhmm}" if hhmm.length == 3
+        raise "Can't parse #{orig.inspect}" unless hhmm.match(/^\d\d\d\d$/)
+
+        hh = hhmm[0,2].to_i
+        mm = hhmm[2,4].to_i
+
+        new hh, mm
+      end
     end
 
+    #
+    # a utility class to match the start and end times of an Event instance, without considering the event date
+    #
     class Interval
       attr_accessor :start, :finish
 
@@ -24,5 +55,6 @@ module Reservation
         start.matches_time?(event.start) && finish.matches_time?(event.finish)
       end
     end
+
   end
 end
--- spec/models/schedule/hour_minute_spec.rb
+++ spec/models/schedule/hour_minute_spec.rb
@@ -18,4 +18,22 @@ describe Reservation::Schedule::HourMinute do
     hm = Reservation::Schedule::HourMinute.new 17, 30
     hm.matches_time?(rightnow).should be_false
   end
+
+  it "should parse '7' as '0700'" do
+    hm = Reservation::Schedule::HourMinute.parse "7"
+    hm.hour.should == 7
+    hm.minute.should == 0
+  end
+
+  it "should parse '11' as '1100'" do
+    hm = Reservation::Schedule::HourMinute.parse "11"
+    hm.hour.should == 11
+    hm.minute.should == 0
+  end
+
+  it "should parse '815' as '0815'" do
+    hm = Reservation::Schedule::HourMinute.parse "815"
+    hm.hour.should == 8
+    hm.minute.should == 15
+  end
 end
